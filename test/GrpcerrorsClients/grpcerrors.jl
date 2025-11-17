8a5519e93d340341c47ccc942d2187210987fd15
Carroll Vance <cvance@medicalmetrics.com>
2025-11-17 06:53:43 -0600

    Removing 0.1.X files

--- test/GrpcerrorsClients/grpcerrors.jl
+++
@@ -1,4 +0,0 @@
-module grpcerrors
-  const _ProtoBuf_Top_ = @static isdefined(parentmodule(@__MODULE__), :_ProtoBuf_Top_) ? (parentmodule(@__MODULE__))._ProtoBuf_Top_ : parentmodule(@__MODULE__)
-  include("grpcerrors_pb.jl")
-end
--- test/RouteguideClients/routeguide.jl
+++
@@ -1,4 +0,0 @@
-module routeguide
-  const _ProtoBuf_Top_ = @static isdefined(parentmodule(@__MODULE__), :_ProtoBuf_Top_) ? (parentmodule(@__MODULE__))._ProtoBuf_Top_ : parentmodule(@__MODULE__)
-  include("route_guide_pb.jl")
-end
