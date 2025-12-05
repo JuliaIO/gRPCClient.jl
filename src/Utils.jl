7004628b5412cf83d330548c54791e12e949fe6e
Carroll Vance <cs.vance@icloud.com>
2025-12-04 21:11:21 -0600

    Make OS specific checks static

--- src/Utils.jl
+++ src/Utils.jl
@@ -4,7 +4,12 @@ function nullstring(x::Vector{UInt8})
     String(x[1:(first_zero_idx-1)])
 end
 
-# On Windows OS_HANDLE does not like curl_sock_t (Int32)
-# Convert it to a pointer instead
-CROSS_PLATFORM_OS_HANDLE(sock::curl_socket_t) =
-    OS_HANDLE(Sys.iswindows() ? Ptr{Cvoid}(Int(sock)) : sock)
+# On Windows x64 OS_HANDLE does not like curl_sock_t (Int32)
+function CROSS_PLATFORM_OS_HANDLE(sock::curl_socket_t)
+    fd = @static if Sys.iswindows()
+        Ptr{Cvoid}(Int(sock))
+    else
+        sock
+    end
+    OS_HANDLE(fd)
+end
